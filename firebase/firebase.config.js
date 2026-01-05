f0ee2676b6927400e2b07063ba2f325a29ceb66b
Juan Ignacio Errecart <jierrecart5@gmail.com>
2026-01-05 12:16:01 -0300

    env add

--- firebase/firebase.config.js
+++ firebase/firebase.config.js
@@ -5,12 +5,12 @@ import {getFirestore} from "firebase/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDT9Z9XOEkk2N2t_6fyRa7ewM7JfMM9Pjw",
-  authDomain: "playbook-ui.firebaseapp.com",
-  projectId: "playbook-ui",
-  storageBucket: "playbook-ui.firebasestorage.app",
-  messagingSenderId: "148407688941",
-  appId: "1:148407688941:web:d5147de1072edced3fb6e4"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
